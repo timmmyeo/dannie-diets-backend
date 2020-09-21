6df3b77b2c97bf82622c9b067fa91c4bfcb26fbc
Timothy Yeo <timothyyeo121@gmail.com>
2020-09-21 12:50:12 +0100

    Back to basics

--- app.py
+++ app.py
@@ -4,28 +4,10 @@ from flask import Flask, request
 from pymessenger.bot import Bot
 import os
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-import json
-import base64
-
-# from dotenv import load_dotenv
-# load_dotenv()
-
-from main_functions import query_wit, update_firestore, get_nutrition
-
-
 app = Flask(__name__)
 ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 bot = Bot(ACCESS_TOKEN)
-
-GOOGLE_FIREBASE_KEY = str.encode(os.environ['GOOGLE_FIREBASE_KEY'])
-cred = credentials.Certificate(json.loads(base64.decodebytes(GOOGLE_FIREBASE_KEY)))
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
 
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
@@ -39,10 +21,9 @@ def receive_message():
     else:
         # get whatever message a user sent the bot
        output = request.get_json()
-       print("======")
-       print("json of request:")
-       print(str(output))
-       print("======")
+       print("=====")
+       print(output)
+       print("=====")
        for event in output['entry']:
           messaging = event['messaging']
           for message in messaging:
@@ -50,18 +31,12 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    user_msg = message['message']['text']
-                    print("The user message that I'm passing in is this: " + user_msg)
-                    process_response(recipient_id, user_msg)
-
+                    response = message['message']['text']
+                    send_message(recipient_id, response)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
-                    # send_message(recipient_id, str(message))
-                    # send_message(recipient_id, message['message']["attachments"][0]["type"])
-                    # send_message(recipient_id, message['message']["attachments"][0]["payload"]["url"])
-                    # response_sent_nontext = get_message()
-                    # send_message(recipient_id, response_sent_nontext)
-                    pass
+                    response_sent_nontext = get_message()
+                    send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
 
@@ -72,43 +47,18 @@ def verify_fb_token(token_sent):
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-#uses PyMessenger to send response to user
-def process_response(recipient_id, user_msg):
-    #sends user the text message provided via input response parameter
-    bot_response = get_response(user_msg)
-    bot.send_text_message(recipient_id, bot_response)
-    return "success"
 
 #chooses a random message to send to the user
-def get_response(user_msg: str) -> str:
-    print("====")
-    print("This is what I typed in: " + user_msg)
-    print("====")
-    resp = query_wit(user_msg)
-    
-    # Doesn't match any intents
-    if len(resp['intents']) == 0:
-        return "That's not something I understand, sorry about that! :( (I'm trying to get smarter every day ;)"
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
 
-    elif resp['intents'][0]['name'] == 'food_ate':
-        # No food entity
-        if 'food:food' not in resp['entities']:
-            return "I'm not really sure what you ate... seems like I need to learn more about the world of humans!"
-        food_ate = resp["entities"]['food:food'][0]['value']
-        try:
-            food_nutrition = get_nutrition(food_ate)
-            update_firestore(user_id="test", db=db, food_nutrition=food_nutrition)
-            return "Seems like you ate: " + str(food_ate) + ". Ah, so you ate food. Nice!"
-        except:
-            print("Error occured somewhere!")
-            return "That food doesn't exist... does it?"
-        
-    elif resp['intents'][0]['name'] == 'nutrition_query':
-        return "So you're interested in your health, great!"
-    else:
-        return "Bippity bop, looks like I'm borked!"
-
-
+#uses PyMessenger to send response to user
+def send_message(recipient_id, response):
+    #sends user the text message provided via input response parameter
+    bot.send_text_message(recipient_id, response)
+    return "success"
 
 if __name__ == "__main__":
     app.run()
